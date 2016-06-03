da82588adf7352825bbde97dd67b4cb71e4f64af
Albert Quer Cufí <albertqc7@gmail.com>
2016-06-03 02:06:15 +0200

    lol

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,6 +15,7 @@ public class MainMenu : MonoBehaviour {
         sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
     }
 	
+
 	// Update is called once per frame
 	void Update () {
 
